c827359905dc7ea2c5db8b897d0152ba5d06aae2
Ilya Zhuravlev <whatever@xyz.is>
2020-03-08 19:34:29 -0400

    Add basic hid code

--- src/main/python/main.py
+++ src/main/python/main.py
@@ -1,11 +1,27 @@
 from fbs_runtime.application_context.PyQt5 import ApplicationContext
-from PyQt5.QtWidgets import QMainWindow
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QWidget, QLabel, QPushButton, QVBoxLayout
 
 import sys
+import hid
+
+class MainWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+        text = QLabel()
+        text.setWordWrap(True)
+        button = QPushButton('Enumerate')
+        button.clicked.connect(lambda: text.setText(str(hid.enumerate())))
+        layout = QVBoxLayout()
+        layout.addWidget(text)
+        layout.addWidget(button)
+        layout.setAlignment(button, Qt.AlignHCenter)
+        self.setLayout(layout)
+
 
 if __name__ == '__main__':
     appctxt = ApplicationContext()       # 1. Instantiate ApplicationContext
-    window = QMainWindow()
+    window = MainWindow()
     window.resize(250, 150)
     window.show()
     exit_code = appctxt.app.exec_()      # 2. Invoke appctxt.app.exec_()
